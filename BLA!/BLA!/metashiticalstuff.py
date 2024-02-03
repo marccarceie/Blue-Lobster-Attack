cb817581217c93cd9b349fc0cc10282c4e724baa
marccarceie <132144486+marccarceie@users.noreply.github.com>
2024-02-03 16:29:08 +0200

    added stuff

--- BLA!/BLA!/metashiticalstuff.py
+++ BLA!/BLA!/metashiticalstuff.py
@@ -4,3 +4,6 @@ def pick():
     print(name,". That is a nice name,",name,"!")
     time.sleep(2)
     return name
+#I've managed to hide from HIM for now.
+#He gave me a key.
+#why does  it say THEREISONLYLOBSTER on the back?
